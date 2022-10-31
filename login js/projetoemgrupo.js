ea5ccae86272bb8d413907e8bb5931644528f4a0
Suelen Moura <113110785+Sumooura@users.noreply.github.com>
2022-10-31 12:16:07 -0300

    Add files via upload

---
+++ login js/projetoemgrupo.js
@@ -0,0 +1,24 @@
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+
+var nome = alert ('Email enviado com sucesso');
+
+
+
+
+
+   
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+   
